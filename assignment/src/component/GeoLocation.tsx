6e53e2dd0bc2b0f84b3bfcc361085088c7d9f821
ashwinip12 <ashwinibms2020@gmail.com>
2024-08-30 15:42:40 +0530

    Provide current location latitude and longitude

---
+++ assignment/src/component/GeoLocation.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const useGeoLocation = () => {
+  const [location, setLocation] = useState<{ lat: number | null; lng: number | null }>({ lat: null, lng: null });
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+ const  [mapsUrl, setMapsUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const success = (position: GeolocationPosition) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      setLocation({ lat, lng });
+      
+     
+      const url = `https://www.google.com/maps?q=${lat},${lng}`;
+      setMapsUrl(url);
+       setLoading(true);
+    };
+                                                                  
+    const errorCallback = (err: GeolocationPositionError) => {
+      setError(err.message);
+      setLoading(false);
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, errorCallback);
+    } else {
+      setError('Geolocation is not supported by your browser.');
+      setLoading(false);
+    }
+  }, []);
+
+  return { location, error, loading,mapsUrl };
+};
+
+export default useGeoLocation;
---
+++ assignment/src/component/LocationComponent.tsx
@@ -0,0 +1,50 @@
+// import React from 'react';
+// import useGeoLocation from './GeoLocation';
+
+
+// function LocationComponent() {
+//   const { location, error, loading } = useGeoLocation();
+//   console.log(loading, error, location)
+//     if(error){
+//     return <>Error</>
+//     }
+//   return (
+//     <div>
+//       <p>Latitude: {location.lat}, Longitude: {location.lng}</p>
+//     </div>
+//   );
+// }
+
+// export default LocationComponent;
+
+import React from 'react';
+import useGeoLocation from './GeoLocation';
+
+
+const LocationComponent: React.FC = () => {
+  const { location, error, loading, mapsUrl } = useGeoLocation();
+console.log(loading, error, location)
+    if(error){
+    return <>Error</>
+    }
+ 
+
+  const handleClick = () => {
+    if (mapsUrl) {
+      window.open(mapsUrl, '_blank', 'noopener,noreferrer');
+    }
+  };
+
+  return (
+    <div>
+      <p>Latitude: {location.lat}, Longitude: {location.lng}</p>
+      {mapsUrl && (
+        <button onClick={handleClick}>
+          View on Google Maps
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default LocationComponent;
